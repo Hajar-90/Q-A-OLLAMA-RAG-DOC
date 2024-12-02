3c16c21928aaa816ac641044c23352fe909b06d1
Hajar-90 <147998132+Hajar-90@users.noreply.github.com>
2024-12-02 11:41:12 +0100

    Initial commit

---
+++ source/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Q/A Doc Application with Ollama and RAG'
+copyright = '2024, Ouled Yahia Hajar'
+author = 'Ouled Yahia Hajar'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,16 @@
+Welcome to the Q-A-OLLAMA-RAG-DOC documentation!
+=================================================
+
+This is the documentation for the Q-A-OLLAMA-RAG-DOC project. Here you can find all the details regarding the setup, usage, and features of this project.
+
+Contents
+---------
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   introduction
+   usage
+   installation
+
+
